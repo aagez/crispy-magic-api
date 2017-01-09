26a18db58c29c7fbe88b0f3f567b5b76281d1424
Narxem <adrien.agez@hotmail.fr>
2017-01-09 21:53:41 +0100

    We can now filter cards by type
    Use the "types" URL parameter with one of the letter that correspond to
    a type (ex : C for creature). Bad letters will bi ignored

--- src/server/api/cards.js
+++ src/server/api/cards.js
@@ -2,29 +2,12 @@
 const express = require('express');
 const router = express.Router();
 const winston = require('winston');
+const util = require('../util.js');
 
 
-function generateFilter(request) {
-  const res =  {fields: {
-      _id: 0,
-      name: 1,
-      code: 1,
-      type: 1,
-      block: 1,
-      translations: 1,
-      cards: {
-        $elemMatch : {
-          name : request["cards.name"]
-        }
-      }
-    }
-  };
-  return res;
-}
 
-router.get('/', (request, response) => {
-  const params = request.query;
-  const dbRequest = {}
+function generateDbRequest(params) {
+  const dbRequest = {cards : {$elemMatch : {}}};
   const dbFilter = {fields: {
       _id: 0,
       name: 1,
@@ -37,21 +20,33 @@ router.get('/', (request, response) => {
       }
     }
   };
-  if (request.query.name !== undefined) {
-    dbRequest["cards.name"] = {
-      $regex: request.query.name,
+  if (params.name !== undefined) {
+    dbRequest.cards.$elemMatch.name = {
+      $regex: params.name,
       $options: 'i'
     }
     dbFilter.fields.cards.$elemMatch.name = {
-      $regex: request.query.name,
+      $regex: params.name,
       $options: 'i'
     }
   }
-  if (request.query.cmc !== undefined && !isNaN(request.query.cmc)) {
-    dbRequest["cards.cmc"] = +request.query.cmc;
-    dbFilter.fields.cards.$elemMatch.cmc = +request.query.cmc;
+  if (params.cmc !== undefined && !isNaN(params.cmc)) {
+    dbRequest.cards.$elemMatch.cmc = +params.cmc;
+    dbFilter.fields.cards.$elemMatch.cmc = +params.cmc;
+  }
+  if (params.types !== undefined) {
+    const types = util.parseTypes(params.types);
+    dbRequest.cards.$elemMatch.types = {$in : types};
+    dbFilter.fields.cards.$elemMatch.types = {$in : types};
   }
 
+  return {dbRequest, dbFilter};
+}
+
+
+
+router.get('/', (request, response) => {
+  const {dbRequest, dbFilter} = generateDbRequest(request.query);
   const cards = request.db.get('en');
   cards.find(dbRequest, dbFilter).then((result) => {
     winston.debug('============= NEW REQUEST =============');
---
+++ src/server/util.js
@@ -0,0 +1,29 @@
+
+const winston = require('winston');
+
+const TYPE_MAP = {
+  "C" : "Creature",
+  "A" : "Artifact",
+  "E" : "Enchantment",
+  "I" : "Instant",
+  "S" : "Sorcery",
+  "P" : "Planeswalker",
+  "L" : "Land",
+  "N" : "Plane",
+  "O" : "Phenomenon"
+}
+
+exports.parseTypes = (param) => {
+  const res = [];
+  var i = 0;
+  for(const letter of param) {
+    if (TYPE_MAP.hasOwnProperty(letter)) {
+      const type = TYPE_MAP[letter];
+      if (res.indexOf(type) === -1)
+        res[i++] = type;
+    }
+    else
+      winston.warn("Unknown type : " + letter);
+  }
+  return res;
+}

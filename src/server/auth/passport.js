efbb42da748f093bd4547d291fadfb758fe16335
Narxem <adrien.agez@hotmail.fr>
2017-01-24 11:59:40 +0100

    refactor passport configuration

--- src/server/auth/passport.js
+++ src/server/auth/passport.js
@@ -34,84 +34,40 @@ passport.ensureAuthenticated = (req, res, next) => {
   res.redirect('/error')
 }
 
+function acceptCredentials(accessToken, refreshToken, profile, done) {
+  winston.debug('looking for user in database')
+  var user = {};
+  users.findOne({provider: profile.provider, id: profile.id})
+    .then(
+      (doc) => { // user found
+        if (doc) {
+          winston.debug('User found : ' + JSON.stringify(doc));
+          user = doc;
+        } else {
+          user = profile;
+          user.token = accessToken;
+          winston.debug('User not found, creating new : ' + JSON.stringify(user));
+          users.insert(user);
+        }
+        done(null, user);
+      }
+    ),
+    (err) => {
+      winston.info('Database error during authentication : ' +  JSON.stringify(err));
+      done(err, null);
+    }
+}
+
 // ####################### CONFIGURE FACEBOOK ##########################
 passport.use(
-  new FacebookStrategy(authenticationConfig.facebook, 
-    (accessToken, refreshToken, profile, done) => {
-      winston.debug('Facebook auth : looking for user in database')
-      var user = {};
-      users.findOne({provider: profile.provider, id: profile.id})
-        .then(
-          (doc) => { // user found
-            if (doc) {
-              winston.debug('Facebook auth : User found : ' + JSON.stringify(doc));
-              user = doc;
-            } else {
-              user = profile;
-              user.token = accessToken;
-              winston.debug('Facebook auth : User not found, creating new : ' + JSON.stringify(user));
-              users.insert(user);
-            }
-            done(null, user);
-          }
-        ),
-        (err) => {
-          winston.info('Database error during authentication : ' +  JSON.stringify(err));
-          done(err, null);
-        }
-    }));
+  new FacebookStrategy(authenticationConfig.facebook, acceptCredentials));
 
 // ####################### CONFIGURE GOOGLE ##########################
 passport.use(
-  new GoogleStrategy(authenticationConfig.google, 
-    (accessToken, refreshToken, profile, done) => {
-      winston.debug('Google auth : looking for user in database')
-      var user = {};
-      users.findOne({provider: profile.provider, id: profile.id})
-        .then(
-          (doc) => { // user found
-            if (doc) {
-              winston.debug('Google auth : User found : ' + JSON.stringify(doc));
-              user = doc;
-            } else {
-              user = profile;
-              user.token = accessToken;
-              winston.debug('Google auth : User not found, creating new : ' + JSON.stringify(user));
-              users.insert(user);
-            }
-            done(null, user);
-          }
-        ),
-        (err) => {
-          winston.info('Database error during authentication : ' +  JSON.stringify(err));
-          done(err, null);
-        }
-    }));
+  new GoogleStrategy(authenticationConfig.google, acceptCredentials));
 
 // ####################### CONFIGURE GOOGLE ##########################
-passport.use(new TwitterStrategy(authenticationConfig.twitter,
-  (token, tokenSecret, profile, done) => {
-    winston.debug('Twitter auth : looking for user in database')
-      var user = {};
-      users.findOne({provider: profile.provider, id: profile.id})
-        .then(
-          (doc) => { // user found
-            if (doc) {
-              winston.debug('Twitter auth : User found : ' + JSON.stringify(doc));
-              user = doc;
-            } else {
-              user = profile;
-              user.token = token;
-              winston.debug('Twitter auth : User not found, creating new : ' + JSON.stringify(user));
-              users.insert(user);
-            }
-            done(null, user);
-          }
-        ),
-        (err) => {
-          winston.info('Database error during authentication : ' +  JSON.stringify(err));
-          done(err, null);
-        }
-    }));
+passport.use(
+  new TwitterStrategy(authenticationConfig.twitter, acceptCredentials));
 
 module.exports = passport;
